0dca6e2b70135c1803bc5d60d2f7344f152b5813
litefeel <litefeel@gmail.com>
2019-06-13 18:56:40 +0800

    Update config.py

--- adbtool/config.py
+++ adbtool/config.py
@@ -3,10 +3,34 @@ import yaml
 from var_dump import var_dump
 
 
-def get_value(key, map):
+def get_value(key, map, default):
     if map is not None:
-        return map.get(key, None)
+        return map.get(key, default)
     return None
+
+
+def get_value_bool(key, map, default):
+    if map is not None:
+        value = map.get(key, default)
+        return value == "true" or value == "True" or value == True
+
+    return False
+
+
+def copy_bool(key, map, obj, default=False):
+    value = get_value_bool(key, map, default)
+    setattr(obj, key, value)
+
+
+def copy_value(key, map, obj, default=None):
+    value = get_value(key, map, default)
+    setattr(obj, key, value)
+
+
+def copy_subconfig(key, map, subconfig):
+    value = get_value(key, map, None)
+    if value is not None:
+        subconfig.load(value)
 
 
 class PushConfig:
@@ -17,28 +41,30 @@ class PushConfig:
         self.paths = []
 
     def load(self, obj):
-        localdir = get_value("localdir", obj)
-        if localdir is not None:
-            self.localdir = localdir
-        remotedir = get_value("remotedir", obj)
-        if remotedir is not None:
-            self.remotedir = remotedir
-        paths = get_value("paths", obj)
-        if paths is not None:
-            self.paths = paths
-        recursion = get_value("recursion", obj)
-        if recursion is not None:
-            self.recursion = recursion == True
+        copy_value("localdir", obj, self, ".")
+        copy_value("remotedir", obj, self, "/sdcard")
+        copy_value("paths", obj, self, [])
+        copy_bool("recursion", obj, self, False)
+
+
+class ApkConfig:
+    def __init__(self):
+        self.apkpath = None
+        self.run = False
+
+    def load(self, obj):
+        copy_value("apkpath", obj, self, None)
+        copy_bool("run", obj, self, False)
 
 
 class Config:
     def __init__(self):
         self.push = PushConfig()
+        self.apk = ApkConfig()
 
     def load(self, obj):
-        push = get_value("push", obj)
-        if push is not None:
-            self.push.load(push)
+        copy_subconfig("push", obj, self.push)
+        copy_subconfig("apk", obj, self.apk)
 
     def load_config(self, filename: str):
         data = read_file(filename)
@@ -49,5 +75,6 @@ class Config:
 
 if __name__ == "__main__":
     cfg = Config()
-    cfg.load_config("test/config.yml")
+    cfg.load_config("tests/config.yml")
+    var_dump(cfg)
 
