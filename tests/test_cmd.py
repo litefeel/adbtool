3e721ea83fa112191eb6932d97e2a4395c5c5d34
litefeel <litefeel@gmail.com>
2019-10-20 08:24:30 +0800

    Update test_cmd.py

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -6,8 +6,3 @@ def test_versionnum(capsys):
     # with capsys.disabled():
     assert versionnum("100.678.9") == 100678009
 
-
-def _assert_success(cmd=None):
-    args = [] if cmd is None else cmd.split()
-    adbtool.main(args)
-
