668dc8daca9e746b1a7760dd3e8d63ab74cf13b0
litefeel <litefeel@gmail.com>
2019-10-22 00:51:58 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,14 +21,32 @@ setup(
     keywords=["Android", "adb"],
     description="A friendly android adb command-line tool",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT Licence",
     url="https://github.com/litefeel/adbtool",
     author="litefeel",
     author_email="litefeel@gmail.com",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
     platforms="any",
     install_requires=["litefeel-pycommon", "PyYAML"],
     scripts=[],
     entry_points={"console_scripts": ["adbt = adbtool.adbtool:main"]},
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production/Stable",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
 )
